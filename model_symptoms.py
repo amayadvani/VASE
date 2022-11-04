62732ee9d82845aab05ef4733ea1be93065e9691
Vivian <vivianni452@gmail.com>
2022-11-03 18:04:20 -0800

    today

--- model_symptoms.py
+++ model_symptoms.py
@@ -1,6 +1,6 @@
-import selenium
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+#import selenium
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
 
 # medical website base address
 medical_website = "https://medlineplus.gov/"
